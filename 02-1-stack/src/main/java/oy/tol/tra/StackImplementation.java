1a97f3c8e4eef40b213bd15c9c38ad1402b1168e
KAzer040 <126124527+KAzer040@users.noreply.github.com>
2024-03-22 21:09:37 +0800

    Add files via upload

--- 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
+++ 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
@@ -1,83 +1,97 @@
 package oy.tol.tra;
 
-/**
- * An implementation of the StackInterface.
- * <p>
- * TODO: Students, implement this so that the tests pass.
- * 
- * Note that you need to implement construtor(s) for your concrete StackImplementation, which
- * allocates the internal Object array for the Stack:
- * - a default constructor, calling the StackImplementation(int size) with value of 10.
- * - StackImplementation(int size), which allocates an array of Object's with size.
- *  -- remember to maintain the capacity and/or currentIndex when the stack is manipulated.
- */
 public class StackImplementation<E> implements StackInterface<E> {
 
-   private Object [] itemArray;
+   private Object[] itemArray;
    private int capacity;
    private int currentIndex = -1;
    private static final int DEFAULT_STACK_SIZE = 10;
 
-   /**
-    * Allocates a stack with a default capacity.
-    * @throws StackAllocationException
-    */
    public StackImplementation() throws StackAllocationException {
-      // TODO: call the constructor with size parameter with default size of 10.
-      
+      this(DEFAULT_STACK_SIZE);
    }
 
-   /** TODO: Implement so that
-    * - if the size is less than 2, throw StackAllocationException
-    * - if the allocation of the array throws with Java exception,
-    *   throw StackAllocationException.
-    * @param capacity The capacity of the stack.
-    * @throws StackAllocationException If cannot allocate room for the internal array.
-    */
    public StackImplementation(int capacity) throws StackAllocationException {
-      
+
+      if (capacity < 2) {
+         throw new StackAllocationException("Capacity must be at least 2.");
+      }
+
+      try {
+         itemArray = new Object[capacity];
+         this.capacity = capacity;
+      } catch (Throwable e) {
+         throw new StackAllocationException("Failed to allocate stack memory.");
+      }
+
    }
 
    @Override
    public int capacity() {
-      // TODO: Implement this
-      
+      return capacity;
+
    }
 
    @Override
    public void push(E element) throws StackAllocationException, NullPointerException {
-      // TODO: Implement this
-               
+
+      if (size() == capacity) {
+         Object[] NEW = new Object[capacity * 5];
+         for (int i = 0; i <= currentIndex; i++) {
+            NEW[i] = itemArray[i];
+         }
+
+         itemArray = NEW;
+         NEW = null;
+         capacity = capacity * 5;
+
+      }
+      if (element == null) {
+         throw new NullPointerException("element is null");
+      }
+      itemArray[++currentIndex] = element;
+
    }
 
    @SuppressWarnings("unchecked")
    @Override
    public E pop() throws StackIsEmptyException {
-      
+      if (isEmpty()) {
+         throw new StackIsEmptyException("Stack is empty");
+      }
+      return (E) itemArray[currentIndex--];
+
    }
 
    @SuppressWarnings("unchecked")
    @Override
    public E peek() throws StackIsEmptyException {
-      
+      if (isEmpty()) {
+         throw new StackIsEmptyException("Stack is empty");
+      }
+      return (E) itemArray[currentIndex];
+
    }
 
    @Override
    public int size() {
-      // TODO: Implement this
-      
+
+      return currentIndex + 1;
+
    }
 
    @Override
    public void clear() {
-      // TODO: Implement this
-      
+
+      currentIndex = -1;
+
    }
 
    @Override
    public boolean isEmpty() {
-      // TODO: Implement this
-      
+
+      return currentIndex == -1;
+
    }
 
    @Override
