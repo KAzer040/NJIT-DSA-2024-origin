6832453738a8fdd816c48f3f369bbc72e5ef3484
KAzer040 <126124527+KAzer040@users.noreply.github.com>
2024-03-24 21:25:31 +0800

    Update Algorithms.java

--- 04-phonebook/src/main/java/oy/tol/tra/Algorithms.java
+++ 04-phonebook/src/main/java/oy/tol/tra/Algorithms.java
@@ -3,35 +3,31 @@ package oy.tol.tra;
 import java.util.function.Predicate;
 
 public class Algorithms {
-    public static <T extends Comparable<T>> void sort(T[] array) {
-        int n = array.length;
-        boolean exchange;
+   public static <T> void reverse(T[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
 
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        boolean sorted;
         do {
-            exchange = false;
-            for (int i = 1; i < n; i++) {
+            sorted = true;
+            for (int i = 1; i < array.length; i++) {
                 if (array[i - 1].compareTo(array[i]) > 0) {
                     T temp = array[i - 1];
                     array[i - 1] = array[i];
                     array[i] = temp;
-                    exchange = true;
+                    sorted = false;
                 }
             }
-            n--;
-        } while (exchange);
-
-    }
-
-    public static <T> void reverse(T[] array) {
-  
-        int i = 0;
-        int x = array.length - 1;
-        while (i <= x / 2) {
-            T temp = array[i];
-            array[i] = array[x - i];
-            array[x - i] = temp;
-            i++;
-        }
+        } while (!sorted);
     }
 
     public static <E extends Comparable<E>> void fastSort(E[] array) {
