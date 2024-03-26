ae07ff3855733c3b4834fa199e2e850602790c09
KAzer040 <126124527+KAzer040@users.noreply.github.com>
2024-03-26 15:38:29 +0800

    Update Grades.java

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -12,30 +12,11 @@ public class Grades {
    }
 
    public void reverse() {
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+     Algorithms.reverse(grades);
    }
 
    public void sort() {
-     int n = grades.length;
-      boolean sort;
-      do{
-         sort = false;
-         for (int i = 1; i < n; i++){
-            if (grades[i-1].compareTo(grades[i])>0){
-               int temp = grades[i-1];
-               grades[i-1] = grades[i];
-               grades[i] = temp;
-               sort = true;
-            }
-         }
-         n--;
-      }while (sort);
+     Algorithms.sort(grades);
    }
 
    public Integer [] getArray() {
